2674e027785847573228b394256987c04c15a878
Rain <iheyu22@163.com>
2024-05-15 23:02:03 +0800

    init

---
+++ src/main/java/com/rainlf/weixinmpserver/WeixinMpServerApplication.java
@@ -0,0 +1,13 @@
+package com.rainlf.weixinmpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinMpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinMpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rainlf/weixinmpserver/WeixinMpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainlf.weixinmpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeixinMpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
