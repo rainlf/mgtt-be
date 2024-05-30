45d368d83c5625a5bd645b1b6e738c60ef15a87b
hy何雨(IT) <yhee@trip.com>
2024-05-30 15:36:56 +0800

    use auto increment id

--- src/main/java/com/rainlf/weixinmpserver/model/User.java
+++ src/main/java/com/rainlf/weixinmpserver/model/User.java
@@ -1,5 +1,7 @@
 package com.rainlf.weixinmpserver.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ import java.time.LocalDateTime;
  */
 @Data
 public class User {
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private String openId;
     private String unionId;
