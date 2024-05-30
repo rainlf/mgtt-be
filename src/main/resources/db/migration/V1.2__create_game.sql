80e7d02732052ad2465167eb84d7823407e06337
hy何雨(IT) <yhee@trip.com>
2024-05-30 16:36:26 +0800

    update

--- src/main/resources/db/migration/V1.2__create_game.sql
+++ src/main/resources/db/migration/V1.2__create_game.sql
@@ -3,10 +3,10 @@ CREATE TABLE `game`
 
     `id`           int          not null auto_increment primary key,
     `game_id`      varchar(128) not null unique,
-    `user_id`      int,
-    `asset_change` int,
-    `type`         varchar(128),
-    `operator_id`  int,
+    `user_id`      int not null,
+    `asset_change` int not null,
+    `type`         varchar(128) not null,
+    `operator_id`  int not null,
     `create_time`  datetime default current_timestamp,
     `update_time`  datetime default current_timestamp on update current_timestamp
 ) ENGINE = InnoDB
