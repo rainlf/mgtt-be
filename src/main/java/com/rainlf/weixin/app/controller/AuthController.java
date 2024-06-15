251ca67b38ae14f78c738037f5255d525aa0d9e9
Rain <iheyu22@163.com>
2024-06-15 18:20:30 +0800

    update

--- src/main/java/com/rainlf/weixin/app/controller/AuthController.java
+++ src/main/java/com/rainlf/weixin/app/controller/AuthController.java
@@ -1,9 +1,9 @@
 package com.rainlf.weixin.app.controller;
 
+import com.rainlf.weixin.app.dto.ApiResp;
 import com.rainlf.weixin.domain.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -18,8 +18,8 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestParam("code") String code) {
+    public ApiResp<String> login(@RequestParam("code") String code) {
         log.info("login code:{}", code);
-        return ResponseEntity.ok(authService.login(code));
+        return ApiResp.success(authService.login(code));
     }
 }
