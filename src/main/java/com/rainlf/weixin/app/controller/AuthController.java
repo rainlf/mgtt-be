f4cfba61aaf8c109ecdeb5038b3e257b14c60d5b
Rain <iheyu22@163.com>
2024-06-20 17:12:35 +0800

    add verifyToken api

--- src/main/java/com/rainlf/weixin/app/controller/AuthController.java
+++ src/main/java/com/rainlf/weixin/app/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.rainlf.weixin.app.controller;
 
 import com.rainlf.weixin.app.dto.ApiResp;
 import com.rainlf.weixin.domain.service.AuthService;
+import com.rainlf.weixin.infra.util.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,5 +27,11 @@ public class AuthController {
     @PostMapping("/mockLogin")
     public ApiResp<String> mockLogin() {
         return ApiResp.success(authService.mockLogin());
+    }
+
+    @GetMapping("/verifyToken")
+    public ApiResp<String> verifyToken(@RequestParam("token") String token) {
+        JwtUtils.getOpenId(token);
+        return ApiResp.success();
     }
 }
