466a6d46b718c10695f807f0431f628172bdef28
rain <iheyu22@gmail.com>
2025-03-14 23:00:06 +0800

    init

---
+++ src/main/java/com/rainlf/mgttbe/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.rainlf.mgttbe.controller;
+
+
+import com.rainlf.mgttbe.controller.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HealthController {
+
+    @GetMapping("ok")
+    public ApiResponse<String> ok() {
+        log.info("ok");
+        return ApiResponse.success("ok");
+    }
+}
